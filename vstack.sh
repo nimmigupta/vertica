9e7e5a8f70937e737218c37eb239acd8c2e5b6b4
Nimmi gupta <nimmi.gupta@gmail.com>
2022-09-20 12:46:58 -0400

    Updated Readme with vstack

---
+++ vstack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in {1..3}; 
+    do
+        echo " Get $i vstack ";
+        for host in `grep -P "^v_" /opt/vertica/config/admintools.conf | awk '{print $3}'| awk -F, '{print $1}'`;
+        do
+            echo " Get vstack of node $host" ;
+            echo " **********************">> /tmp/vstack_$host_$i.log;
+            echo " $i Vstack Node $host ">> /tmp/vstack_$host_$i.log;
+            echo " **********************">> /tmp/vstack_$host_$i.log;
+            ssh $host "/opt/vertica/bin/vstack">>/tmp/vstack_$host_$i.log;
+        done;
+        sleep 300;
+    done;
